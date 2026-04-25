ce0149f7fd1166733bd1783660c1dec7b385ac11
Developer <developer@example.com>
2026-04-25 18:04:34 +0530

    add all changes to git

--- utils/schema_validator.py
+++
@@ -1,9 +0,0 @@
-import json
-
-from jsonschema import validate
-
-def validate_schema(response, schema_path):
-    with open(schema_path) as file:
-        schema = json.load(file)
-        
-    validate(instance=response.json(), schema=schema)
